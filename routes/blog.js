f578dc10e0f0744607a8b3bfc26bbcea4eda4866
YouHao <haoyou0807@163.com>
2020-03-04 11:15:30 +1000

    router development

--- routes/blog.js
+++ routes/blog.js
@@ -21,17 +21,17 @@ router.get('/list', (req, res, next) => {
   let author = req.query.author || '';
   const keyword = req.query.keyword || '';
 
-  // if (req.query.isadmin) {
-  //   // 管理员界面
-  //   const loginCheckResult = loginCheck(req);
-  //   if (loginCheckResult) {
-  //     // 未登录
-  //     return loginCheckResult;
-  //   }
-  //   // 强制查询自己的博客
-  author = req.session.username;
-  // }
-
+  if (req.query.isadmin) {
+    // 管理员界面
+    const loginCheckResult = loginCheck(req);
+    if (loginCheckResult) {
+      // 未登录
+      return loginCheckResult;
+    }
+    // 强制查询自己的博客
+    author = req.session.username;
+    // }
+  }
   const result = getList(author, keyword);
   return result.then(listData => {
     res.json(new SuccessModel(listData));
